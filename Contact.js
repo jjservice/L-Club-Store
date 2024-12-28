b0add8beafe8c32294fb0d13db1b3cc6511de335
jjservice <jjfabian47@gmail.com>
2024-12-28 00:06:11 -0500

    usy765

--- Contact.js
+++ Contact.js
@@ -1,14 +1,14 @@
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyCBDffMAaJdgb1_Xsfa4zhoYLYAhxmrZVo",
-    authDomain: "contact-form-9868a.firebaseapp.com",
-    databaseURL: "https://contact-form-9868a-default-rtdb.firebaseio.com",
-    projectId: "contact-form-9868a",
-    storageBucket: "contact-form-9868a.firebasestorage.app",
-    messagingSenderId: "686565340119",
-    appId: "1:686565340119:web:8cbc0e2cfc7772af449192"
-  };
+  apiKey: "AIzaSyCN_k4GjMD5p3rXnHBhqgfaV-U7S8D-Ivw",
+  authDomain: "contact-form-f2b2a.firebaseapp.com",
+  databaseURL: "https://contact-form-f2b2a-default-rtdb.firebaseio.com",
+  projectId: "contact-form-f2b2a",
+  storageBucket: "contact-form-f2b2a.firebasestorage.app",
+  messagingSenderId: "517314640927",
+  appId: "1:517314640927:web:be20935bebba9ceed8292a"
+};
   
   // Initialize Firebase
   firebase.initializeApp(firebaseConfig);
